c1524b6e833c638d903ce7677fe609cfeeb19b91
Mahdi <mahdi.a.200066@gmail.com>
2020-03-30 17:45:14 +0430

    Added Custom broadcasting events

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+let socket = io();
+
+socket.on('connect', function(){
+    console.log('Connected to server.');
+    
+});
+socket.on('disconnect', function(){
+    console.log('Disconnected from server.');
+    
+});
+socket.on('newMessage', function(message){
+    console.log('newMessage:', message);
+    
+});
--- server/server.js
+++ server/server.js
@@ -7,19 +7,41 @@ const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
 
 let app = express();
-let server = http.createServer(app);
+let server = http.createServer(app);    
 let io = SocketIO(server);
 
 app.use(express.static(publicPath));
 
 io.on("connection", function(socket){
     console.log('a new user just connected.');
+
+    socket.emit('newMessage',{
+        from: 'Admin',
+        text: 'welcome to the chat',
+    })
+    
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime(),
+    });
+
     socket.on('disconnect', function(){
         console.log('User disconnected.');
         
     });
+
+    socket.on('createMessage', function(message){
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime(),
+        }); 
+
+    })
 });
 
-server.listen(3000, function(){
+server.listen(port, function(){
     console.log(`Server is up on port ${port}`);
 });
